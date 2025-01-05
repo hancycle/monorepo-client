2378bb58d2eb454e6332f91d1a843d9bb15987ca
sungzzn <msj9121@naver.com>
2025-01-05 20:26:42 +0900

    feat: 바텀시트 컴포넌트 추가

--- apps/ggoody-native/app/(tabs)/index.tsx
+++ apps/ggoody-native/app/(tabs)/index.tsx
@@ -1,16 +1,32 @@
 import { Image, StyleSheet, Platform, Text, View } from "react-native";
 import { WebView } from "react-native-webview";
-
+import { useRef } from "react";
 import Animated from "react-native-reanimated";
 import Button from "@hancycle/ui/components/react/Button";
+import BottomSheet, {
+  BottomSheetModal,
+} from "@hancycle/ui/components/expo/BottomSheet";
 
 export default function HomeScreen() {
+  const bottomSheetRef = useRef<BottomSheetModal>(null);
+
   return (
     <View style={styles.container}>
       <Animated.ScrollView>
         <Animated.View>
           <Text>Home</Text>
-          <Button>Button</Button>
+          <Button
+            onClick={() => {
+              bottomSheetRef.current?.present();
+            }}
+          >
+            바텀시트 버튼
+          </Button>
+          <BottomSheet bottomSheetRef={bottomSheetRef}>
+            <View>
+              <Text>BottomSheet</Text>
+            </View>
+          </BottomSheet>
           <WebView
             source={{ uri: "http://192.168.219.111:5173" }}
             style={styles.webview}
---
+++ packages/ui/components/expo/BottomSheet.tsx
@@ -0,0 +1,66 @@
+import {
+  BottomSheetModal,
+  BottomSheetView,
+  BottomSheetModalProvider,
+  BottomSheetBackdrop,
+} from "@gorhom/bottom-sheet";
+import { useRef, useCallback, ReactNode, RefObject } from "react";
+import { StyleSheet } from "react-native";
+
+type BottomSheetProps = {
+  children: ReactNode; // 바텀시트 내부 컨텐츠
+  trigger?: ReactNode; // 바텀시트를 열 트리거 컴포넌트 (선택적)
+  bottomSheetRef?: RefObject<BottomSheetModal>;
+};
+
+function BottomSheet({ children, trigger, bottomSheetRef }: BottomSheetProps) {
+  const innerRef = useRef<BottomSheetModal>(null);
+  const targetRef = bottomSheetRef || innerRef; // 외부 ref가 없으면 내부 ref 사용
+
+  const handleSheetChanges = useCallback((index: number) => {
+    console.log("handleSheetChanges", index);
+  }, []);
+
+  // renders
+  const renderBackdrop = useCallback(
+    (props) => (
+      <BottomSheetBackdrop
+        {...props}
+        // appearsOnIndex={0}
+        // disappearsOnIndex={-1}
+        pressBehavior="close" // 백드롭 클릭 시 바텀시트 닫기
+        style={[
+          {
+            backgroundColor: "rgba(0, 0, 0, 0.5)",
+          },
+        ]}
+      />
+    ),
+    []
+  );
+
+  return (
+    <>
+      {trigger && trigger}
+      <BottomSheetModal
+        ref={targetRef}
+        snapPoints={["25%", "50%", "75%"]}
+        // backdropComponent={renderBackdrop}
+        onChange={handleSheetChanges}
+      >
+        <BottomSheetView style={styles.container}>{children}</BottomSheetView>
+      </BottomSheetModal>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+  },
+});
+
+export { BottomSheetModalProvider, BottomSheetView, BottomSheetModal };
+
+export default BottomSheet;
